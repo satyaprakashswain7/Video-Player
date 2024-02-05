2db85097ab1ca698f4ddb039351ae3a08efd28a8
satya prakash swain <satyasitu14@gmail.com>
2024-02-05 21:58:05 +0530

    added custom Api response

--- src/index.js
+++ src/index.js
@@ -6,3 +6,11 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server running at port ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGODB connection error:",err);
+})
